df6d4db18e100a3bbb8898c597777042a04a6449
Leonard Kevin McGuire Jr <kmcg3413@gmail.com>
2015-11-16 10:49:46 -0600

    prep for usage with fm2db project

--- src/ham.rs
+++ src/ham.rs
@@ -38,14 +38,13 @@ pub use dsp::FileSource;
 
 pub use usrp::USRPSource;
 
-fn main() {        
+pub fn router(rtrans: Arc<Mutex<Vec<Vec<f32>>>>) {        
+    println!("[ham-router] initializing");
     let taps: Vec<f32> = vec![0.44378024339675903f32, 0.9566655158996582, 1.4999324083328247, 2.0293939113616943, 2.499887466430664, 2.8699963092803955, 3.106461763381958, 3.1877646446228027, 3.106461763381958, 2.8699963092803955, 2.499887466430664, 2.0293939113616943, 1.4999324083328247, 0.9566655158996582, 0.44378024339675903];
 
     let sps = 4000000.0;    
     
-    let mut fmdemod0 = FMDemod::new(sps, 10, -840000.0, 15000.0, taps, 3);
-
-    println!("processing");
+    let mut fmdemod0 = FMDemod::new(sps, 10, -440000.0, 15000.0, taps, 3);
     
     let mut buf: Vec<f32> = Vec::new();
     
@@ -54,8 +53,6 @@ fn main() {
     let mut ausrp = USRPSource::new(sps, 146000000.0, 70.0);
     let mut usrp = ausrp.lock().unwrap();
     //let mut usrp = FileSource::new(String::from_str("/home/kmcguire/Projects/radiowork/usbstore/recording01").unwrap());   
-    
-    println!("usrp created");
     
     let mut total_samps = 0;
 
@@ -67,35 +64,15 @@ fn main() {
     let mut skip: usize = 0;
     
     let gst = time::precise_time_ns() as f64 / 1000.0 / 1000.0 / 1000.0;
-    //while buf.len() < 16000 * 15 {
+    
+    println!("[ham-router] listening to broadband signal");
+
     loop {
         let mut ibuf = usrp.recv();
-        
-        /*
-        for x in 0..ibuf.len() {
-            sum.i += ibuf[x].i;
-            sum.q += ibuf[x].q;
-            sumcnt += 1;
-        }
-
-        if (sumcnt > 4000000) {
-            sum.i = sum.i / sumcnt as f32;
-            sum.q = sum.q / sumcnt as f32;
-            sumcnt = 1;
-        }        
-        
-        for x in 0..ibuf.len() {
-            ibuf[x].i = ibuf[x].i - sum.i / sumcnt as f32;
-            ibuf[x].q = ibuf[x].q - sum.q / sumcnt as f32;
-        }
-        */
-        
+                
         total_samps += ibuf.len();
-        //println!("decoding block");
         let st = time::precise_time_ns() as f64 / 1000.0 / 1000.0 / 1000.0;
-        let mut out = fmdemod0.work(&ibuf);
-        
-        //println!("out.len:{}", out.len());   
+        let mut out = fmdemod0.work(&ibuf);   
         
         for x in 0..out.len() {
             if out[x].abs() > 0.0 {
@@ -116,18 +93,22 @@ fn main() {
                 buf.push(out[x]);     
             } else {
                 if buf.len() / 16000 > 2 {
-                    println!("saved {}", buf.len() / 16000);
-                    wavei8write(format!("rec_{}.wav", (st - gst).floor()), 16000, &buf);
+                    //println!("saved {}", buf.len() / 16000);
+                    //wavei8write(format!("rec_{}.wav", (st - gst).floor()), 16000, &buf);
+                    
+                    // Place the transmission into the output buffer. The locking
+                    // provides the synchronization to support an external thread
+                    // accessing the buffer concurrently.
+                    println!("[ham-router] placed transmission of {} seconds into output buffer", buf.len() as f32 / 16000.0);
+                    let mut out = rtrans.lock().unwrap();
+                    out.push(buf);
+                    
+                    buf = Vec::new();
+                } else {
+                    buf.clear();
                 }
-                buf.clear();
             }
         }        
-
-        /*
-        for x in 0..out.len() {
-            std::io::stderr().write_f32::<LittleEndian>(out[x] * 12.5);
-        } 
-        */       
         
         if total_samps > 4000000 * 2 {
             total_samps = 0;
@@ -139,14 +120,11 @@ fn main() {
         //println!("total_samps:{} time:{}", total_samps, st as f64 / 1000.0 / 1000.0 / 1000.0);
     }
     
-    let gsec = (time::precise_time_ns() as f64 - gst as f64) / 1000.0 / 1000.0 / 1000.0;
-    println!("total_samps:{} total_time:{} samps_per_second:{}", total_samps, gsec, total_samps as f64 / gsec); 
-    
-    println!("total_samps/sps:{}", total_samps as f32 / 4000000.0);
-    
-    wavei8write(String::from_str("tmp.wav").unwrap(), 16000, &buf);
-    
-    println!("done");
+    //let gsec = (time::precise_time_ns() as f64 - gst as f64) / 1000.0 / 1000.0 / 1000.0;
+    //println!("total_samps:{} total_time:{} samps_per_second:{}", total_samps, gsec, total_samps as f64 / gsec); 
+    //println!("total_samps/sps:{}", total_samps as f32 / 4000000.0);    
+    //wavei8write(String::from_str("tmp.wav").unwrap(), 16000, &buf);
+    //println!("done");
 }
 
 
